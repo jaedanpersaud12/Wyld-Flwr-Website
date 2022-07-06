002a6860b3eaa54a9019899d409082680ccbac8d
Jaedan Persaud <63131031+jaedanpersaud12@users.noreply.github.com>
2022-07-06 12:11:02 -0400

    init

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
-const withVideos = require("next-videos");
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ["raw.githubusercontent.com"],
-  },
-};
+}
 
-module.exports = { nextConfig, withVideos };
+module.exports = nextConfig
